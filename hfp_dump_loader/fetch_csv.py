378fbc6c6d06525cfec286a202e0cde726ed5e95
keripukki <arttu.kosonen@gmail.com>
2021-03-19 10:02:59 +0200

    Fix chunk size

--- hfp_dump_loader/fetch_csv.py
+++ hfp_dump_loader/fetch_csv.py
@@ -31,7 +31,7 @@ def get_csv_chunk(req_iterator, chunk_size):
     """Fetch max `chunk_size` rows from a `requests.get(..., stream=True).iter_lines()` iterator."""
     assert chunk_size >= 1, 'chunk_size must be at least 1'
     rows = []
-    for i in range(chunk_size-1):
+    for i in range(chunk_size):
         next_row = next(req_iterator)
         if next_row:
             rows.append(next_row)
