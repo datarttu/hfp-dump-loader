392ae3ab97911275242b4647f49005c2bc983d5a
keripukki <arttu.kosonen@gmail.com>
2021-03-19 14:00:09 +0200

    Csv field handling WIP

--- hfp_dump_loader/fetch_csv.py
+++ hfp_dump_loader/fetch_csv.py
@@ -4,11 +4,10 @@ Fetch, filter and save CSV data from the storage.
 
 import sys
 import csv
-import pytz
 import requests
 import xmltodict
-from datetime import datetime
 from .utils import TYPE_CASTS
+from .utils import cast_failing_to_none
 
 def get_csv_chunk(req_iterator, chunk_size):
     """Fetch max `chunk_size` rows from a `requests.get(..., stream=True).iter_lines()` iterator."""
@@ -28,3 +27,30 @@ def split_fields(str_rows, expected_n_fields):
     rows = [el.split(',') for el in str_rows]
     rows = [el for el in rows if len(el) == expected_n_fields]
     return rows
+
+def transpose_to_cols_dict(list_rows, field_mapping):
+    """Transpose `list_rows` into list of columns, select and name the columns
+    by `field_mapping`."""
+    list_cols = list(map(list, zip(*list_rows)))
+
+    idx_name_pairs = []
+    for k, v in field_mapping.items():
+        if isinstance(v, dict):
+            idx_name_pairs.append((k, v['name']))
+        else:
+            idx_name_pairs.append((k, v))
+
+    cols = {v: list_cols[k] for k, v in idx_name_pairs}
+    return cols
+
+def cast_col_types(cols_dict, field_mapping):
+    """Return `cols_dict` with column data types changed according to `field_mapping`.
+    If no type cast is defined, the column is left as string."""
+    to_cast = {v['name']: TYPE_CASTS[v['type']] for k, v in field_mapping.items() if isinstance(v, dict)}
+    for name, func in to_cast.items():
+        cols_dict[name] = [cast_failing_to_none(el, func) for el in cols_dict[name]]
+    return cols_dict
+
+def transpose_to_dict_rows(cols_dict):
+    """Return `cols_dict` as list of dict rows."""
+    return [dict(zip(cols_dict.keys(), row)) for row in zip(*cols_dict.values())]
--- hfp_dump_loader/utils.py
+++ hfp_dump_loader/utils.py
@@ -1,14 +1,17 @@
 """Utility functions and constants."""
 
+import pytz
+from datetime import datetime
+
 def milliseconds_to_timestamp(x):
     """Convert Unix milliseconds `x` into an UTC timestamp value."""
-    return datetime.fromtimestamp(x / 1000.0, tz=pytz.utc)
+    return datetime.fromtimestamp(float(x) / 1000.0, tz=pytz.utc)
 
 def milliseconds_to_date(x):
     """Convert Unix seconds `x` into a date value."""
     # NOTE: We have to check the results for oday validity,
     #       there may be a caveat with UTC / Europe/Helsinki tz...
-    return datetime.fromtimestamp(x / 1000.0, tz=pytz.utc).date()
+    return datetime.fromtimestamp(float(x) / 1000.0, tz=pytz.utc).date()
 
 # Available type cast names for config.yml and their respective functions.
 TYPE_CASTS = {
@@ -17,3 +20,10 @@ TYPE_CASTS = {
     'timestamp': milliseconds_to_timestamp,
     'date': milliseconds_to_date
 }
+
+def cast_failing_to_none(x, f):
+    """Return `f(x)`, returning `None` on any error."""
+    try:
+        return f(x)
+    except:
+        return None
