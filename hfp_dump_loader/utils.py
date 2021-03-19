97da8e5a6caf0d3f13e2ef558add0c39e26b1700
keripukki <arttu.kosonen@gmail.com>
2021-03-19 14:33:26 +0200

    Fix int cast to allow decimal source values

--- hfp_dump_loader/utils.py
+++ hfp_dump_loader/utils.py
@@ -3,19 +3,23 @@
 import pytz
 from datetime import datetime
 
+def str_float_to_int(x):
+    """Convert string like '123.0' to an integer."""
+    return int(float(x))
+
 def milliseconds_to_timestamp(x):
-    """Convert Unix milliseconds `x` into an UTC timestamp value."""
+    """Convert Unix milliseconds `x` to an UTC timestamp value."""
     return datetime.fromtimestamp(float(x) / 1000.0, tz=pytz.utc)
 
 def milliseconds_to_date(x):
-    """Convert Unix seconds `x` into a date value."""
+    """Convert Unix seconds `x` to a date value."""
     # NOTE: We have to check the results for oday validity,
     #       there may be a caveat with UTC / Europe/Helsinki tz...
     return datetime.fromtimestamp(float(x) / 1000.0, tz=pytz.utc).date()
 
 # Available type cast names for config.yml and their respective functions.
 TYPE_CASTS = {
-    'int': int,
+    'int': str_float_to_int,
     'float': float,
     'timestamp': milliseconds_to_timestamp,
     'date': milliseconds_to_date
